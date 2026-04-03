b263c9a5471256d510b00f8163107c20b5e10f0f
Mike Morakis <michalismorakis@gmai.com>
2026-04-03 16:06:08 +0300

    Add Εμπορικά Σήματα and Ανακοπές categories, assign 32 posts

---
+++ assign_categories.sql
@@ -0,0 +1,39 @@
+-- Εμπορικά Σήματα (id=17) - registration, filing, domain, general trademark posts
+UPDATE posts SET category_id = 17 WHERE slug IN (
+  'katoxurwsi-domain-name-kai-emporiko-sima',
+  'kataxwrisi-onoma-katoxurosi-sima-logo',
+  'kataxorisi-simatos-elegxos',
+  'onoma-simata-kataxorisi-dikigoros',
+  'emporiko-sima-dikigoros-kataxorisi-katoxurosi',
+  'katoxirosi-simatos-dhlwsh',
+  'EUIPO-OBI-SIMA-ELEGXOS-DIKIGOROS',
+  'sima-epwnumia-domain-kataxwrisi',
+  'OBI-SIMATA-EUIPO',
+  'emporiko-sima-kataxwrish',
+  'emporiko-sima-eidikos',
+  'dikaio-simatwn',
+  'emporiko-sima-dikigoroi',
+  'emporikosimaseminario',
+  'branding-marketing',
+  'sima-tis-europaikis-enwsis'
+);
+
+-- Ανακοπές (id=18) - opposition, infringement, confusion risk, famous marks, litigation
+UPDATE posts SET category_id = 18 WHERE slug IN (
+  'paraviasi-sima-dikigoros-prostasia',
+  'sima-dikigoros-prosvoli-katoxurosi',
+  'prostasia-sima-trademark',
+  'prosvoli-emporikou-simatos',
+  'kataxwrisi-sima-euipo',
+  'emporiko-sima-EE-EUIPO',
+  'trademark-simata-dikigoros-juv',
+  'trademark-netflix-euipo',
+  'emporiko-sima-onoma-turkaegean-euipo-katoxurosi',
+  'russia-polemos-trademark',
+  'emporiko-sima-patentes-polemos',
+  'adwords-google-trademark',
+  'european-intellectual-property-review',
+  'kluwer-trademark-blog-wolters-kluwer-nv',
+  'journal-of-intellectual-property-law',
+  'blog-post'
+);
